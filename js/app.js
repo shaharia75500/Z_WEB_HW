256fdbb8ddd48abd9ba61c1219d5a737233206eb
shaharia75500 <103421420+shaharia75500@users.noreply.github.com>
2023-02-20 22:38:34 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-$(function () {
-   'use strict'
-});
+
 
 $(function () {
+   'use strict'
    $('.bannerSlider').slick ({
       prevArrow : '<i class="fa-solid fa-circle-chevron-left slider_arrow_left"></i>',
       nextArrow : '<i class="fa-solid fa-circle-chevron-right slider_arrow_right"></i>',
@@ -43,4 +42,18 @@ $(function () {
    delay: 30,
    time: 1000,
    });
+
+   // initializing filterizer js
+   $('.gallery-filter-container').filterizr();
+
+   var selector = '#gallery-filter-control li';
+
+   // CODE EXPLANATION
+   // At first it removes all active class and 
+   // then it adds active class to the clicked list item
+   $(selector).on('click', function(){
+      $(selector).removeClass('active');
+      $(this).addClass('active');
+   });
+
 });
